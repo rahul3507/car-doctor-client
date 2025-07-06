916bff687ff171c3933e30cc85b53922d5e07a56
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-06 13:05:36 +0600

    Refactor Footer component and integrate it into Main layout; remove old Footer implementation

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/common/footer/Footer";
 
 const Main = () => {
   return (
     <div>
       <Outlet />
+      <Footer />
     </div>
   );
 };
