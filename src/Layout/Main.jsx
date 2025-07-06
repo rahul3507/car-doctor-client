025ec950342884d8f75b3dca9f1bdd6cbb5cf8f5
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-06 14:41:45 +0600

    Refactor code structure for improved readability and maintainability

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,10 +3,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/common/footer/Footer";
+import Navbar from "../components/common/navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
