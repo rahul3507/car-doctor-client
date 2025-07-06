8337c640aad35d1509f9a1bc5ee7f4e4d089ca23
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-06 12:21:27 +0600

    Implement main layout component and update routing to use Main component

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default Main;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,12 @@
 /** @format */
 
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
   },
 ]);
 
