5166fc9645cc0479839fb15833a7efa13b01d013
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-07 10:37:20 +0600

    feat: add Banner component with Carousel for enhanced visual display

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,11 +4,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/common/footer/Footer";
 import Navbar from "../components/common/navbar/Navbar";
+import Banner from "../pages/Home/banner/Banner";
 
 const Main = () => {
   return (
     <div>
       <Navbar />
+      <Banner />
       <Outlet />
       <Footer />
     </div>
