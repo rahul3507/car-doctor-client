72e09632a34cd8013e02c4b9b7602f7736dcd4eb
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-08 14:53:00 +0600

    feat: implement Services component with initial structure for service offerings

---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="mt-4">
+      <div className="text-center">
+        <h3>Services</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
