f3722c0fa849e6273a175c72d38b3d283946d41c
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-08 12:32:24 +0600

    feat: update About component layout and replace placeholder image with banner images

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -4,13 +4,19 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="hero bg-base-200 ">
+    <div className="hero h-fit bg-base-200 ">
       <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
+        <div className="w-full  md:w-3/4 lg:w-1/2 relative  ">
+          <img
+            src="about-banner-1.png"
+            className="w-3/5 h-72 md:h-[360px] lg:h-[460px] rounded-lg shadow-2xl"
+          />
+          <img
+            src="about-banner-2.png"
+            className="w-1/2 h-72 md:h-80 lg:h-96 border-4 lg:border-8 border-gray-50 absolute right-28 top-1/3 rounded-lg shadow-2xl"
+          />
+        </div>
+        <div className="w-full md:w-3/4 lg:w-1/2 space-y-6">
           <h1 className="text-5xl font-bold">Box Office News!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
