33f7e1399b0d64607c45aadcae21b240c40f6f30
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-09 11:39:07 +0600

    firebase aded

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+/** @format */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDizGy8NAAsvhNjy32Jbn_5XFrQsgLZQMg",
+  authDomain: "cars-doctor-64c31.firebaseapp.com",
+  projectId: "cars-doctor-64c31",
+  storageBucket: "cars-doctor-64c31.firebasestorage.app",
+  messagingSenderId: "881666392462",
+  appId: "1:881666392462:web:6b747c964b20ea104fa4df",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
