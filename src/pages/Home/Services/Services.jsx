dc3508518cb0d7e6059e042d59c29381c8907458
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-09 08:24:27 +0600

    updated services

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React, { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
@@ -23,7 +24,11 @@ const Services = () => {
           believable.
         </p>
       </div>
-      <div>{services.length}</div>
+      <div>
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service} />
+        ))}
+      </div>
     </div>
   );
 };
