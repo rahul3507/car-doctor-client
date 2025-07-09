9f467c4c6b2304f27888da703046dc0d067f5bb7
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-09 11:16:24 +0600

    login page updated

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,6 +1,9 @@
 /** @format */
 
 const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="hero h-screen  w-full">
       <div className="hero-content w-full  flex-col lg:flex-row">
@@ -9,17 +12,27 @@ const Login = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <h1 className="text-3xl text-center font-bold">Login</h1>
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
+            <h1 className="text-3xl text-center font-bold dark:text-white">
+              Login
+            </h1>
+            <form onSubmit={handleLogin}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4" type="submit">
+                  Login
+                </button>
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>
