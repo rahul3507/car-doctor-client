b71141b17b785034cc426d2fe1845e7602a56631
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-10 13:02:33 +0600

    background greadiant aded

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,49 +1,53 @@
 /** @format */
 
 import React from "react";
+import { BackgroundGradient } from "../../../components/ui/background-gradient";
 import {
   CardBody,
   CardContainer,
   CardItem,
 } from "../../../components/ui/3d-card";
+
 import { MoveRight, MoveUpRight } from "lucide-react";
 
 const ServiceCard = ({ service }) => {
   const { title, img, price } = service;
   return (
     <CardContainer className="inter-var m-0">
-      <CardBody className=" dark:bg-gray-700  relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]  dark:border-white/[0.2] border-black/[0.1]  max-w-[30rem] h-auto rounded-xl p-6 pt-3 border  ">
-        <CardItem translateZ="100" className="w-full mt-4">
-          <img
-            src={img}
-            height="1000"
-            width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
-        </CardItem>
-        <CardItem
-          translateZ="50"
-          className="text-lg md:text-xl  font-bold  dark:text-white  mt-3"
-        >
-          {title}
-        </CardItem>
+      <BackgroundGradient className="">
+        <CardBody className=" dark:bg-gray-700  relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]  dark:border-white/[0.2] border-black/[0.1]  max-w-[30rem] h-auto rounded-xl p-6 pt-3 border  ">
+          <CardItem translateZ="100" className="w-full mt-4">
+            <img
+              src={img}
+              height="1000"
+              width="1000"
+              className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+              alt="thumbnail"
+            />
+          </CardItem>
+          <CardItem
+            translateZ="50"
+            className="text-lg md:text-xl  font-bold  dark:text-white  mt-3"
+          >
+            {title}
+          </CardItem>
 
-        <div className="flex justify-between items-center mt-2">
-          <CardItem
-            translateZ="50"
-            className="text-sm md:text-base font-medium text-red-400"
-          >
-            Price: {price}
-          </CardItem>
-          <CardItem
-            translateZ="50"
-            className="text-sm md:text-base font-medium text-red-500 cursor-pointer"
-          >
-            <MoveRight />
-          </CardItem>
-        </div>
-      </CardBody>
+          <div className="flex justify-between items-center mt-2">
+            <CardItem
+              translateZ="50"
+              className="text-sm md:text-base font-medium text-red-400"
+            >
+              Price: {price}
+            </CardItem>
+            <CardItem
+              translateZ="50"
+              className="text-sm md:text-base font-medium text-red-500 cursor-pointer"
+            >
+              <MoveRight />
+            </CardItem>
+          </div>
+        </CardBody>
+      </BackgroundGradient>
     </CardContainer>
   );
 };
