15db0e92f53aca27968648f8caae516cfea59bc2
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-13 04:45:55 -0600

    useLoader used to checkout

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,11 +1,14 @@
 /** @format */
 
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const CheckOut = () => {
+  const service = useLoaderData();
+  const { title, _id } = service;
   return (
     <div>
-      <h1>check out</h1>
+      <h1>Car Services : {title}</h1>
     </div>
   );
 };
