3ab21cd04881c296d4389b04996e7e4cd263d051
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-13 23:27:39 -0600

    checkout form updated

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -5,25 +5,50 @@ import { useLoaderData } from "react-router-dom";
 
 const CheckOut = () => {
   const service = useLoaderData();
-  const { title, _id } = service;
+  const { title, _id, price } = service;
   return (
-    <div>
+    <div className="text-center justify-center items-center">
       <h1>Car Services : {title}</h1>
-      <div className="card-body">
+      <div className="card-body text-center justify-center items-center">
         <form>
-          <fieldset className="fieldset">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+          <fieldset className="fieldset  ">
+            <div className="  grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="flex flex-col gap-2">
+                <label className="label">Name</label>
+                <input
+                  type="name"
+                  name="name"
+                  className="input w-64 md:w-72 "
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="label ">Date</label>
+                <input
+                  type="date"
+                  name="date"
+                  className="input w-64 md:w-72"
+                  placeholder="date"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input w-64 md:w-72"
+                  name="email"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="label">Deu amount</label>
+                <input
+                  type="text"
+                  className="input  w-64 md:w-72"
+                  defaultValue={price + `$`}
+                />
+              </div>
             </div>
             <button className="btn btn-primary  btn-block" type="submit">
-              Login
+              Confirm Order
             </button>
           </fieldset>
         </form>
