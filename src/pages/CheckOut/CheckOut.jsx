7fce36f545cdea0e13bcf47bfb0f4437d964e268
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-13 22:43:14 -0600

    form aded

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -11,12 +11,18 @@ const CheckOut = () => {
       <h1>Car Services : {title}</h1>
       <div className="card-body">
         <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <form>
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
 
-          <button className="btn btn-neutral mt-4">Login</button>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </form>
         </fieldset>
       </div>
     </div>
