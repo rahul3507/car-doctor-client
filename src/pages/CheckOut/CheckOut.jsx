f92e4edc2acc3af6eb08ff74a62acb2785d9d6f4
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-13 22:48:31 -0600

    checkout form updated

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -10,20 +10,23 @@ const CheckOut = () => {
     <div>
       <h1>Car Services : {title}</h1>
       <div className="card-body">
-        <fieldset className="fieldset">
-          <form>
-            <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
-            <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
-            <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
-            <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
-
-            <button className="btn btn-neutral mt-4">Login</button>
-          </form>
-        </fieldset>
+        <form>
+          <fieldset className="fieldset">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+            </div>
+            <button className="btn btn-primary  btn-block" type="submit">
+              Login
+            </button>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
