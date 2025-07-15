a1275a3309a94ecc5c7af844bd6c087b97df3df3
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-14 23:38:19 -0600

    added privateroute and added table to bookings

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import { useContext } from "react";
+import { AuthContext } from "../pages/Providers/Providers";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+  if (user?.email) {
+    return children;
+  }
+  return <Navigate to="/login" replace></Navigate>;
+};
+
+export default PrivateRoute;
