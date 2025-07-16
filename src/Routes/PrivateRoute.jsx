782228917b1b55be4b87d98f9120d44db648a777
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-15 22:53:09 -0600

    added uselocation to the private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -2,10 +2,12 @@
 
 import { useContext } from "react";
 import { AuthContext } from "../pages/Providers/Providers";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
 
   if (loading) {
     return <progress className="progress w-56"></progress>;
